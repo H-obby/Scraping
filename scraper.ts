7b70f367c8fc21b9fd7c85e7a4588842446471db
Hugo Joseph <hugo.joseph@optimix.fr>
2025-02-20 14:35:49 +0100

    Start

--- scraper.ts
+++ scraper.ts
@@ -1,6 +1,12 @@
 import { default as axios } from 'axios';
 import * as cheerio from 'cheerio';
 import { promises } from 'dns';
+import { readFile, readFileSync, writeFile } from 'fs';
+import * as csvWriter from 'csv-writer';
+import { parse } from 'csv-parse';
+import * as path from 'path';
+
+type FeatMap = { [K: string]: { [K: string]: string } };
 
 async function fetch_html(url: string): Promise<string> {
   try {
@@ -13,10 +19,48 @@ async function fetch_html(url: string): Promise<string> {
   }
 }
 
-fetch_html('https://www.gemmaline.com/dons/dons-abjurateur-d-armure-1270.htm').then(html => {
-  const $ = cheerio.load(html);
-  let name = $('body > h1').clone().children().remove().end().text().trim()
-  let type = $('body > h1 > em').text();
+async function get_feats(url: string, feats_data: FeatMap): Promise<FeatMap>{
+  fetch_html('https://gemmaline.com/dons'+url).then(html => {
+    const $ = cheerio.load(html);
+    let name = $('body h1').clone().children().remove().end().text().trim();
+    let type = $('body h1 em').text();
+    
+    feats_data[name] = {
+      'name': name,
+      'type': type
+    };
+    
+    console.log(feats_data)
+  });
 
-  console.log(name,'\n', type);
+  return feats_data;;
+}
+
+fetch_html('https://gemmaline.com/dons/dons-general.htm').then(async html => {
+  const $ = cheerio.load(html);
+
+  let feats_url = $('body table.tableau tbody tr td strong a')
+    .map((_, element) => $(element).attr('href')?.substring(1))
+    .get();
+
+  let feats_data: FeatMap = {};
+
+  for(var url of feats_url.slice(0, 1)){
+    await fetch_html('https://gemmaline.com/dons'+url).then(html => {
+      const $ = cheerio.load(html);
+      let nom = $('body h1').clone().children().remove().end().text().trim();
+      let type = $('body h1 em').text();
+      let courte_desc = $('body p:first').clone().children().remove().end().text().split('\n')[1].trim();
+      let source = $('.source').text()
+      
+      feats_data[nom] = {
+        'nom': nom,
+        'type': type,
+        'courte_description': courte_desc,
+        'source': source,
+      };
+    });
+  };
+
+  console.log(feats_data)
 });
